abc82a4e1a83e2e768315e4f201d6c885b64bc9a
Trias Bratayudhana <triasbrata@gmail.com>
2022-01-03 19:10:29 +0700

    u: proxy server

--- example/remoteServer.js
+++ example/remoteServer.js
@@ -3,9 +3,9 @@ const { proxyServer } = require('../index.js');
 /* internet server proxy configuration */
 
 proxyServer({
-  proxyPort: 9898,
+  proxyPort: 80,
   tunnelPort: 8010,
   // Uncomment below to enable transport encryption::
-  //encKey: 'DwUuKDoFtHVWrWGfS4rz1pm7bOTZ988o',
-  //encIv: '435435f432543532'
+  encKey: 'triasbrata',
+  encIv: 'trias@123'
 });
